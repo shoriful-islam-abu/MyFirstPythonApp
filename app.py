7f4526e0b106ca523d6a9e24fff90bf8363f140e
shoriful-islam-abu <150802534+shoriful-islam-abu@users.noreply.github.com>
2023-11-15 07:40:39 +0000

    added requirements file

--- app.py
+++ app.py
@@ -2,33 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-content = [
-  {
+content = [{
     'id': '1000',
     'name': 'Mahmud Akhter',
     'position': 'Human Resources'
-  },
-  {
+}, {
     'id': '1001',
     'name': 'Tahmina Rahman',
     'position': 'Network Administration'
-  },
-  {
+}, {
     'id': '1002',
     'name': 'John Maritt',
     'position': 'Develpoment'
-  },
-  {
+}, {
     'id': '1003',
     'name': 'John Maritt',
     'position': 'Software Architecture'
-  },
-  {
+}, {
     'id': '1004',
     'name': 'Conner Ardin',
     'position': 'Human Resources'
-  }
-]
+}]
+
 
 @app.route("/")
 def hello():
